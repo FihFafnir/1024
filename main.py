840ae1df6358f4f567ca77d96842c172252e5f08
FihFafnir <fihfdragon@gmail.com>
2023-04-21 00:20:32 -0300

    Refactored

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import re
 
 def do_cipher_of_caesar(text, key, just_letters):
     encrypted_text = ""
         
     for char in text:
-        num = ord(char)
-        if (num >= 65 and num <= 90) or (num >= 97 and num <= 122) or not just_letters:
+        if re.match(r"[A-Za-z]", char) or not just_letters:
             num = ord(char)
             char = chr(num + key)
         encrypted_text += char
@@ -16,18 +16,18 @@ encrypted_texts = []
 
 for i in number_of_lines:
     text = input()
-    encrypted_text = do_cipher_of_caesar(text, 3, True)
+    first_phrase_encrypted_text = do_cipher_of_caesar(text, 3, True)
 
-    encrypted_text = encrypted_text[::-1]
+    second_phrase_encrypted_text = first_phrase_encrypted_text[::-1]
 
-    half_text_index = len(encrypted_text)//2
-    first_half_text = encrypted_text[:half_text_index]
-    second_half_text = encrypted_text[half_text_index:]
+    half_text_index = len(second_phrase_encrypted_text)//2
+    first_half_text = second_phrase_encrypted_text[:half_text_index]
+    second_half_text = second_phrase_encrypted_text[half_text_index:]
 
     second_half_text = do_cipher_of_caesar(second_half_text, -1, False)
     
-    encrypted_text = first_half_text + second_half_text
-    encrypted_texts.append(encrypted_text)
+    third_phrase_encrypted_text = first_half_text + second_half_text
+    encrypted_texts.append(third_phrase_encrypted_text)
     
 for encrypted_text in encrypted_texts:
     print(encrypted_text)
