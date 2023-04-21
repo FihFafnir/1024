61b6ccd8794c9ed69687d847c14f3b77bd9efa58
FihFafnir <fihfdragon@gmail.com>
2023-04-21 00:34:12 -0300

    Variable name changes

--- main.py
+++ main.py
@@ -12,22 +12,22 @@ def do_cipher_of_caesar(text, key, just_letters):
     return encrypted_text
 
 number_of_lines = range(int(input()))
-encrypted_texts = []
+encrypted_lines = []
 
 for i in number_of_lines:
-    text = input()
-    first_phrase_encrypted_text = do_cipher_of_caesar(text, 3, True)
+    line = input()
+    first_phrase_encrypted_line = do_cipher_of_caesar(line, 3, True)
 
-    second_phrase_encrypted_text = first_phrase_encrypted_text[::-1]
+    second_phrase_encrypted_line = first_phrase_encrypted_line[::-1]
 
-    half_text_index = len(second_phrase_encrypted_text)//2
-    first_half_text = second_phrase_encrypted_text[:half_text_index]
-    second_half_text = second_phrase_encrypted_text[half_text_index:]
+    half_line_index = len(second_phrase_encrypted_line)//2
+    first_half_line = second_phrase_encrypted_line[:half_line_index]
+    second_half_line = second_phrase_encrypted_line[half_line_index:]
 
-    second_half_text = do_cipher_of_caesar(second_half_text, -1, False)
+    second_half_line = do_cipher_of_caesar(second_half_line, -1, False)
     
-    third_phrase_encrypted_text = first_half_text + second_half_text
-    encrypted_texts.append(third_phrase_encrypted_text)
+    third_phrase_encrypted_line = first_half_line + second_half_line
+    encrypted_lines.append(third_phrase_encrypted_line)
     
-for encrypted_text in encrypted_texts:
-    print(encrypted_text)
+for encrypted_line in encrypted_lines:
+    print(encrypted_line)
